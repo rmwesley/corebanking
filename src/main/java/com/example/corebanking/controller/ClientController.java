9bb55a451274a11c6bd5bd1031995a2005045db8
Wesley <wesley.machado.2018@polytechnique.org>
2023-05-18 10:30:44 +0200

    Updated and documented ClientController endpoints

--- src/main/java/com/example/corebanking/controller/ClientController.java
+++ src/main/java/com/example/corebanking/controller/ClientController.java
@@ -1,9 +1,6 @@
 package com.example.corebanking.controller;
 
 import java.util.List;
-import java.util.Optional;
-
-import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,29 +16,34 @@ import com.example.corebanking.dto.ClientCreationDTO;
 import com.example.corebanking.model.Client;
 import com.example.corebanking.repository.ClientRepository;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/clients")
+@Tag(name = "Clients API")
 public class ClientController {
 
   @Autowired private ClientRepository clientRepository;
 
+  @Operation(summary = "Get list of all clients")
   @GetMapping
   private ResponseEntity<List<Client>> getClients() {
     return new ResponseEntity<>(clientRepository.findAll(), HttpStatus.OK);
   }
 
+  @Operation(summary = "Get client by id")
   @GetMapping("/{clientId}")
   private ResponseEntity<Client> getClient(@PathVariable Long clientId) {
-    Optional<Client> optAccount = clientRepository.findById(clientId);
-    if (!optAccount.isPresent()) {
-      throw new EntityNotFoundException("Client with id " + clientId + " not found.\n");
-    }
-    return new ResponseEntity<>(optAccount.get(), HttpStatus.OK);
+    Client client = clientRepository.getReferenceById(clientId);
+    return new ResponseEntity<>(client, HttpStatus.OK);
   }
 
+  @Operation(summary = "Creates and adds a client to the database")
   @PostMapping
-  public ResponseEntity<Long> createClient(@RequestBody ClientCreationDTO clientCreationDTO) {
-    Client saved = clientRepository.saveAndFlush(new Client(clientCreationDTO.getName()));
-    return new ResponseEntity<>(saved.getId(), HttpStatus.CREATED);
+  public ResponseEntity<Client> createClient(@RequestBody ClientCreationDTO clientCreationDTO) {
+    Client client = new Client(clientCreationDTO.getName());
+    clientRepository.saveAndFlush(client);
+    return new ResponseEntity<>(client, HttpStatus.CREATED);
   }
 }
