d9df0e53ce28452386e28a890e4c754b81555ae6
Wesley <wesley.machado.2018@polytechnique.org>
2023-05-18 10:25:03 +0200

    Renamed and removed unused methods

--- src/main/java/com/example/corebanking/model/Client.java
+++ src/main/java/com/example/corebanking/model/Client.java
@@ -26,14 +26,6 @@ public class Client implements IHistory {
     this.name = name;
   }
 
-  public void createAccount() {
-    accounts.add(new Account());
-  }
-
-  public void createAccount(double initialBalance) {
-    accounts.add(new Account(initialBalance));
-  }
-
   public void addAccount(Account acc) {
     accounts.add(acc);
   }
@@ -50,7 +42,7 @@ public class Client implements IHistory {
     return this.accounts;
   }
 
-  public double getTotalBalance() {
+  public double calculateTotalBalance() {
     return this.accounts.stream().mapToDouble(Account::getBalance).sum();
   }
 
